b388d9f2bede96280021685a33865c051a46f95f
Olivier Raymond <olivier.raymond176@gmail.com>
2022-11-24 08:09:47 -0500

    ajouter une classe

---
+++ src/Car.js
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+class Car extends Component {
+   render() {
+      return(
+         <div>
+            <h1>Une voiture</h1>
+         </div>
+      );
+   }
+}
+export default Car;
