29ef151dd02b618a5d5d35603389fd4040a7b081
Olivier Raymond <olivier.raymond176@gmail.com>
2022-11-24 08:48:07 -0500

    ajouter bouton

--- src/Car.js
+++ src/Car.js
@@ -1,20 +1,37 @@
 import {Component} from "react";
 
 class Car extends Component {
+   state = {
+      couleurSt:'rouge', marqueSt:'Reh', kmSt:'100 km'
+   }
+   onCouleurChange = (e) => {
+      this.setState({couleurSt: e.target.value});
+   }
+   onMarqueChange = (e) => {
+      this.setState({marqueSt: e.target.value});
+   }
+   onKmChange = (e) => {
+      this.setState({kmSt: e.target.value});
+   }
    render() {
       console.log(this.props)
       return(
          <div>
             <h1>Une voiture</h1>
             <p>
-               Entrer couleur : <input type="text" placeholder="couleur"/>
+               Entrer couleur : <input type="text" placeholder="couleur" onChange = {this.onCouleurChange}/>
             </p>
             <p>
-               Entrer marque : <input type="text" placeholder="marque"/>
+               Entrer marque : <input type="text" placeholder="marque" onChange = {this.onMarqueChange}/>
             </p>
             <p>
-               Entrer kilométrage : <input type="text" placeholder="kilométrage"/>
+               Entrer kilométrage : <input type="text" placeholder="kilométrage" onChange = {this.onKmChange}/>
             </p>
+            <button onClick={this.changerVoiture}></button><br/>
+            <p>la couleur est {this.state.couleurSt}</p>
+            <p>la marque est {this.state.marqueSt}</p>
+            <p>le kilométrage est{this.state.kmSt}</p>
+
          </div>
       );
    }
