0a7813fc6e2e54df39850c798bd7e33ee02191ba
Olivier Raymond <olivier.raymond176@gmail.com>
2022-11-24 08:10:45 -0500

    ajouter car dans App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import {Component} from "react";
+import Car from "./Car";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
   return (
     <div className="App">
       <h1>Exercice 3</h1>
+      <Car />
     </div>
   );
   }
