7bdc2ae687e5a241f23f0b6905b1366a338e5b2a
Olivier Raymond <olivier.raymond176@gmail.com>
2022-11-24 08:07:50 -0500

    transformen App en classe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import {Component} from "react";
 
-function App() {
+class App extends Component {
+  render() {
+
+
   return (
     <div className="App">
       <h1>Exercice 3</h1>
     </div>
   );
+  }
 }
 
 export default App;
